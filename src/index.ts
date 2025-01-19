5b4ac0c197b2d4192185dcd6381b6c0fc5739872
Marcos Iván Alvarez <alvmivan@gmail.com>
2025-01-19 05:56:05 -0300

    move index

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-﻿export * from "./hooks/useFieldState";
-export * from "./hooks/useElementState";
-export * from "./hooks/useEmptyState";
-export * from "./hooks/useFieldState";
-export * from "./hooks/useRemapArray";
-export * from "./hooks/useStateObject";
-export * from "./stateObject";
-
-
