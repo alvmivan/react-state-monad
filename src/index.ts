856a506e88eba1afcfec4524307d26c1206c367d
Marcos Iván Alvarez <alvmivan@gmail.com>
2025-01-19 05:52:39 -0300

    exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
-﻿import {StateObject} from "./stateObject";
+﻿export * from "./hooks/useFieldState";
+export * from "./hooks/useElementState";
+export * from "./hooks/useEmptyState";
+export * from "./hooks/useFieldState";
+export * from "./hooks/useRemapArray";
+export * from "./hooks/useStateObject";
+export * from "./stateObject";
 
-// an empty index just to make tsc happy
+
